ed15a8b2e172ead875c080ada6c4f771089cb7a9
Yngve Levinsen <yngve.levinsen@ess.eu>
2022-02-10 21:25:20 +0100

    styling update (black)

--- setup.py
+++ setup.py
@@ -6,21 +6,21 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(
-     name='pychiver',
-     version='0.7.1',
-     author="Arek Gorzawski",
-     author_email="arek.gorzawski@ess.eu",
-     description="A python utility for EPICS archiver, SaveAndRestore and waveform handling",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     install_requires=[
-          'requests',
-          'python-dateutil',
-          'pandas',
-          'matplotlib',
-          'numpy',
-          'pyepics',
-#          'epicsarchiver'
-     ],
-     packages=setuptools.find_packages(),
- )
+    name="pychiver",
+    version="0.7.1",
+    author="Arek Gorzawski",
+    author_email="arek.gorzawski@ess.eu",
+    description="A python utility for EPICS archiver, SaveAndRestore and waveform handling",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "requests",
+        "python-dateutil",
+        "pandas",
+        "matplotlib",
+        "numpy",
+        "pyepics",
+        #          'epicsarchiver'
+    ],
+    packages=setuptools.find_packages(),
+)
