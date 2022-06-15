a4a7d2416f1ffbc387a09f2a150268c1ce38a909
arek <arek.gorzawski@ess.eu>
2022-06-15 20:16:39 +0200

    version++

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pychiver",
-    version="0.7.1",
+    version="0.7.2",
     author="Arek Gorzawski",
     author_email="arek.gorzawski@ess.eu",
     description="A python utility for EPICS archiver, SaveAndRestore and waveform handling",
